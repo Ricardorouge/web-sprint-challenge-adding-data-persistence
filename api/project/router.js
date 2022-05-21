dc9b93e409c59698dae60d922759cb69c8994036
Ricardorouge <ricky032929@gmail.com>
2022-05-20 23:27:40 -0400

    projects get endpoint done

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,20 @@
 // build your `/api/projects` router here
+const express = require('express')
+const Projects = require('./model')
+const router = express.Router()
+
+router.get('/',(req,res,next)=>{
+    Projects.getALL()
+    .then(result=>{
+        result.map(project=>{
+            project.project_completed === 0?
+            project.project_completed = false :
+            project.project_completed = true
+        })
+        res.json(result)
+    })
+    .catch(next)
+})
+
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -9,9 +9,9 @@ const server = express()
 
 
 server.use(express.json())
-// server.use('/api/projects',projectRouter)
+server.use('/api/projects',projectRouter)
 server.use('/api/resources',resourceRouter)
-// server.use('api/tasks',taskRouter)
+// server.use('/api/tasks',taskRouter)
 
 
 module.exports = server
