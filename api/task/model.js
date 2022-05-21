14a1c9f7651b503b389627b99a090301e3949822
Ricardorouge <ricky032929@gmail.com>
2022-05-20 23:52:16 -0400

    tasks get endpoint finished

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,29 @@
 // build your `Task` model here
+const db = require('../../data/dbConfig')
+
+function getALL(){
+    return db('tasks as t')
+    .join('projects as p','p.project_id','t.project_id')
+    .select('t.*','p.project_name','p.project_description')
+}
+
+function getById(id){
+    return db('tasks as t')
+    .join('projects as p')
+    .select('t.*','p.project_name','p.project_description')
+    .where({task_id:id})
+    .first()
+}
+
+function add(task){
+    db('tasks').insert(task)
+    .then(result=>{
+        return getById(result)
+    })
+}
+
+module.exports = {
+    getALL,
+    getById,
+    add,
+}
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,19 @@
 // build your `/api/tasks` router here
+const express = require('express')
+const Tasks = require('./model')
+const router = express.Router()
+
+router.get('/',(req,res,next)=>{
+    Tasks.getALL()
+    .then(result=>{
+        result.map( task=>{
+            task.task_completed === 0?
+            task.task_completed = false :
+            task.task_completed = true
+        })
+        res.json(result)
+    })
+    .catch(next)
+})
+
+module.exports = router
