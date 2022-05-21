096b8efb0b60f1ec14f95110dd0561870fecac5e
Ricardorouge <ricky032929@gmail.com>
2022-05-20 23:16:48 -0400

    projects model

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,26 @@
 // build your `Project` model here
+const db = require('../../data/dbConfig')
+
+function getALL(){
+    return db('projects')
+}
+function getById(id){
+    return db('projects').where({project_id:id}).first()
+}
+function getByName(name){
+    return db('projects').where({project_name:name}).first()
+}
+
+function add(project){
+    return db('projects').insert(project)
+    .then(result=>{
+        return getById(result)
+    })
+}
+
+module.exports = {
+    getALL,
+    getById,
+    getByName,
+    add
+}
